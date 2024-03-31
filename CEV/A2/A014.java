1386e66e9258b369d5eb680c745fc60dbe9ff5bd
André Wanderley <154604167+Andrewanport@users.noreply.github.com>
2024-03-31 17:27:53 -0300

    Update A014.java

--- CEV/A2/A014.java
+++ CEV/A2/A014.java
@@ -5,7 +5,21 @@
 
 package CEV.A2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A014 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o valor em Celsius: ");
+        float Celsius = entrada.nextFloat();
+
+        float Fahrenheit = Celsius * 1.8f + 32f;
+
+        System.out.printf("Valor em celsius: %.2f\n", Celsius);
+        System.out.printf("Valor em celsius: %.2f", Fahrenheit);
+
+    }
 }
